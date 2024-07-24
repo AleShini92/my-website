4f6022a838637ee690466dffeed9bbc8ef8508ae
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-07-24 19:25:46 +0200

    created grid for photo works

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,16 +1,7 @@
 function mySkills() {
-    //document.getElementById("my-skills").style.display = "block";
 
     var skill = document.getElementById("my-skills");
     var about = document.getElementById("myPortfolio");
-
-    // if (skill.style.display === "none" || about.style.display === "block") {
-    //     skill.style.display = "block"; // Show content
-    //     about.style.display = "none";
-    // } else {
-    //     skill.style.display = "none"; // Hide content
-    //     about.style.display = "block";
-    // }
 
     if (skill.style.display === "none") {
         skill.style.display = "block"; // Show content
