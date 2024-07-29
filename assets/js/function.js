2ca561c69ca2f2409e2a8e6b842a326dffcbe654
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-07-29 21:21:26 +0200

    fixed form

--- assets/js/function.js
+++ assets/js/function.js
@@ -31,31 +31,6 @@ function portfolio() {
 }
 
 
-/*
-*
-* button scroll top
-*
-*/
-
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 
 /*
 *
@@ -98,4 +73,30 @@ function removeClass() {
   element.classList.remove("overflow");
   // L'ancoraggio al link avviene automaticamente senza ulteriori azioni
   checkbox.checked = "";
+}
+
+
+/*
+*
+* button scroll top
+*
+*/
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
