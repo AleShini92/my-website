bc1864b38c22c1a4825ac47cdec42fef25c6cfc0
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-08-01 11:25:45 +0200

    fixed img width about

--- assets/js/function.js
+++ assets/js/function.js
@@ -82,25 +82,45 @@ function removeClass() {
 *
 */
 
-document.addEventListener('DOMContentLoaded', () => {
-  const myButton = document.getElementById('myBtn');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const myButton = document.getElementById('myBtn');
 
-  function scrollFunction() {
-      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-          myButton.style.display = 'block';
-      } else {
-          myButton.style.display = 'none';
-      }
+//   function scrollFunction() {
+//       if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//           myButton.style.display = 'block';
+//       } else {
+//           myButton.style.display = 'none';
+//       }
+//   }
+
+//   function topFunction() {
+//       document.body.scrollTop = 0;
+//       document.documentElement.scrollTop = 0;
+//   }
+
+//   window.scrollFunction = scrollFunction; // Esponi la funzione scrollFunction globalmente
+//   window.topFunction = topFunction; // Esponi la funzione topFunction globalmente
+
+//   // Inizializza il pulsante
+//   scrollFunction(); // Assicura che il pulsante sia mostrato/nascosto all'inizio
+// });
+
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+}
 
-  function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-  }
-
-  window.scrollFunction = scrollFunction; // Esponi la funzione scrollFunction globalmente
-  window.topFunction = topFunction; // Esponi la funzione topFunction globalmente
-
-  // Inizializza il pulsante
-  scrollFunction(); // Assicura che il pulsante sia mostrato/nascosto all'inizio
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
