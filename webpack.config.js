82496428b7841821d0f1661857a8a13439649604
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-11-13 23:39:22 +0100

    fixed fav:safari

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
-const path = require('path');
+import path from 'path';
 
-module.exports = {
-  devtool: 'eval-source-map',
-  entry: {
-    main: './src/index.js',
-  },
-  
-  mode: 'development',
-  
-  module: {
-    rules: [{
-      exclude: /node_modules/,
-      use:
-      [{
-        loader: 'babel-loader'
-      }],
- test: /\.jsx?$/
+export const devtool = 'eval-source-map';
+export const entry = {
+  main: './src/index.js',
+};
+export const mode = 'development';
+export const module = {
+  rules: [{
+    exclude: /node_modules/,
+    use: [{
+      loader: 'babel-loader'
+    }],
+    test: /\.jsx?$/
+  }, {
+    use: [{
+      loader: 'style-loader'
     }, {
-      use:
-      [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader'
-      }, {
-        loader: 'sass-loader'
-      }],
-      test: /\.s[ac]ss$/i
-    }]
-  },
-  
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].bundle.js',
-  },
+      loader: 'css-loader'
+    }, {
+      loader: 'sass-loader'
+    }],
+    test: /\.s[ac]ss$/i
+  }]
+};
+export const output = {
+  path: __dirname + '/dist',
+  filename: '[name].bundle.js',
+  plugins: [
+    new WebpackFavicons({
+      src: 'public/favicon.svg',
+      path: 'img',
+      background: '#000',
+      theme_color: '#000',
+      icons: {
+        favicons: true
+      }
+    })
+  ]
 };
